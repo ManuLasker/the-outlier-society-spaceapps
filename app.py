da27387a8008e1f1bf1b121623c24032b4d2a659
Emanuel Isaac Afanador Pacheco <eiafanad@bancolombia.com.co>
2024-10-06 13:57:46 -0500

    first commit

---
+++ app.py
@@ -0,0 +1,277 @@
+import streamlit as st
+from datetime import datetime
+from st_aggrid import AgGrid, GridOptionsBuilder
+from src.data_utils import (load_data, get_list_of_data_training,
+                            get_match_filename_training, load_special_file, plot_values_from_special_content,
+                            apply_bandpass_filter, apply_highpass_filter, apply_lst_sta_algo_and_plot_char_func,
+                            calculate_on_off_cft_and_plot, plot_real_trigger_axvline, calculate_arrival_time, 
+                            calculate_predicted_arrival_time, calculate_root_mean_square_error,
+                            get_list_of_data_testing)
+
+def training_data_visualization_review():
+    import streamlit as st
+
+    folder_selection = st.selectbox(label="Select what data you want to see", options=["mars", "lunar"])
+
+    list_data_df = get_list_of_data_training(folder_selection, "csv")
+
+    # Configure grid options using GridOptionsBuilder
+    list_data_df_builder = GridOptionsBuilder.from_dataframe(list_data_df)
+    list_data_df_builder.configure_pagination(enabled=True)
+    list_data_df_builder.configure_selection(selection_mode='single', use_checkbox=False)
+    grid_options = list_data_df_builder.build()
+
+    # Display AgGrid
+    st.write("List of Data for Mars/Lunar")
+    list_data_df_return_value = AgGrid(list_data_df, gridOptions=grid_options)
+
+    if list_data_df_return_value["selected_data"] is not None:
+        selected_value = list_data_df_return_value["selected_data"]["path"].values[0]
+        selected_df = load_data(selected_value)
+        extension = "mseed"
+        match_filename = get_match_filename_training(selected_df["filename"].to_list(), folder_selection, extension)
+
+        st.write(f"List of Data for selected value: {selected_value}")
+
+        # Configure grid options using GridOptionsBuilder
+        selected_df_builder = GridOptionsBuilder.from_dataframe(selected_df)
+        selected_df_builder.configure_pagination(enabled=True)
+        selected_df_builder.configure_selection(selection_mode='single', use_checkbox=False)
+        grid_options = selected_df_builder.build()
+        selected_df_return_value = AgGrid(selected_df, gridOptions=grid_options)
+
+        # Plot current data
+        if selected_df_return_value["selected_data"] is not None:
+            selected_value = selected_df_return_value["selected_data"]["filename"].values[0]
+            selected_arrival_time = selected_df_return_value["selected_data"]["time_abs(%Y-%m-%dT%H:%M:%S.%f)"].values[0]
+            selected_arrival_time = datetime.strptime(selected_arrival_time, '%Y-%m-%dT%H:%M:%S.%f')
+            st.write(f"Plot of the time series for file {selected_value}")
+            file_name_to_analyze = match_filename[selected_value]
+            selected_sample_st = load_special_file(file_name_to_analyze)
+            fig, ax = plot_values_from_special_content(selected_sample_st, file_name_to_analyze, selected_arrival_time, plot_real=True)
+            st.pyplot(fig, clear_figure=False)
+
+            # Set Up filters
+            st.write("High Pass Configuration")
+            left, right = st.columns(2)
+            freq_input = left.text_input(label="freq", value="2")
+            corners_input = right.text_input(label="corners", value="4")
+            st.write("Band Pass Configuration")
+            left, right = st.columns(2)
+            min_freq = left.text_input(label="min_freq", value="0.4")
+            max_freq = right.text_input(label="max_freq", value="1.0")
+            last_data_st, cft = None, None
+
+            left, middle, right = st.columns(3)
+
+            if left.button("apply highpass", use_container_width=True):
+                hp_st_data = apply_highpass_filter(selected_sample_st, float(freq_input), int(corners_input))
+                fig, _ = plot_values_from_special_content(hp_st_data, file_name_to_analyze, selected_arrival_time, "High Pass Filter", plot_real=True, plot_spectogram=True)
+                st.session_state["fig"] = fig
+                st.session_state["last_data_st"] = hp_st_data.copy()
+
+            if middle.button("apply bandpass", use_container_width=True):
+                bp_st_data = apply_bandpass_filter(selected_sample_st, float(min_freq), float(max_freq))
+                fig, _ = plot_values_from_special_content(bp_st_data, file_name_to_analyze, selected_arrival_time, "Band Pass Filter", plot_real=True, plot_spectogram=True)
+                st.session_state["fig"] = fig
+                st.session_state["last_data_st"] = bp_st_data.copy()
+
+            if right.button("hp + bp", use_container_width=True):
+                hp_st_data = apply_highpass_filter(selected_sample_st, float(freq_input), int(corners_input))
+                bp_st_data = apply_bandpass_filter(hp_st_data, float(min_freq), float(max_freq))
+                fig, _ = plot_values_from_special_content(bp_st_data, file_name_to_analyze, selected_arrival_time, "HP + BP Filter", plot_real=True, plot_spectogram=True)
+                st.session_state["fig"] = fig
+                st.session_state["last_data_st"] = bp_st_data.copy()
+
+            if st.session_state.get("fig"):
+                st.pyplot(st.session_state.get("fig"), clear_figure=False)
+
+                # Apply algorithm STA/LTA
+                st.write("STA/LTA Configuration")
+                left, right = st.columns(2)
+                sta_len = left.text_input(label="sta len", value="120")
+                lta_len = right.text_input(label="lta len", value="600")
+                if st.button("Apply STA/LTA", use_container_width=True):
+                    if st.session_state.get("last_data_st") is not None:
+                        cft, fig, ax = apply_lst_sta_algo_and_plot_char_func(st.session_state.get("last_data_st"),
+                                                                             float(sta_len), float(lta_len))
+                        st.session_state["cft"] = cft
+                        st.session_state["fig_cft"] = fig
+
+                if st.session_state.get("fig_cft"):
+                    st.pyplot(st.session_state.get("fig_cft"), clear_figure=False)
+
+                    st.write("On/Off points configuration")
+                    left, right = st.columns(2)
+                    trh_on = left.text_input(label="thr on", value="4")
+                    trh_off = right.text_input(label="thr off", value="1.5")
+                    if st.button("Calculate On/Off points", use_container_width=True):
+                        if st.session_state.get("cft") is not None:
+                            triggers, fig, ax = calculate_on_off_cft_and_plot(st.session_state.get("last_data_st"),
+                                                                              st.session_state.get("cft"),
+                                                                              float(trh_on), float(trh_off))
+                            fig, ax = plot_real_trigger_axvline(st.session_state.get("last_data_st"), selected_arrival_time, ax, fig)
+
+                            if not triggers:
+                                st.warning("Method could not detect the triggers change parameters")
+
+                            st.session_state["triggers"] = triggers
+                            st.pyplot(fig, clear_figure=False)
+                            if triggers:
+                                st.write("Error between the actual arrival time and detection")
+                                left, right = st.columns(2)
+                                real_arrival_time = calculate_arrival_time(st.session_state.get('last_data_st'), selected_arrival_time)
+                                predicted_arrival_time = calculate_predicted_arrival_time(st.session_state.get('last_data_st'), triggers)
+                                left.write(f"Actual: {real_arrival_time}")
+                                right.write(f"Detected: {predicted_arrival_time}")
+                                st.write(f"MSE: {calculate_root_mean_square_error(predicted_arrival_time, real_arrival_time)}")
+
+
+def testing_data_visualization_review():
+    import streamlit as st
+
+    folder_selection = st.selectbox(label="Select what test data you want to see", options=["mars", "lunar"])
+
+    list_data_df = get_list_of_data_testing(folder_selection, "mseed")
+
+    # Configure grid options using GridOptionsBuilder
+    list_data_df_builder = GridOptionsBuilder.from_dataframe(list_data_df)
+    list_data_df_builder.configure_pagination(enabled=True)
+    list_data_df_builder.configure_selection(selection_mode='single', use_checkbox=False)
+    grid_options = list_data_df_builder.build()
+
+    # Display AgGrid
+    st.write("List of Data for Mars/Lunar")
+    list_data_df_return_value = AgGrid(list_data_df, gridOptions=grid_options)
+
+    if list_data_df_return_value["selected_data"] is not None:
+        selected_value = list_data_df_return_value["selected_data"]["path"].values[0]
+        file_name_to_analyze = selected_value
+
+        # Plot current data
+        st.write(f"Plot of the time series for file {selected_value}")
+        selected_sample_st = load_special_file(file_name_to_analyze)
+        fig, ax = plot_values_from_special_content(selected_sample_st, file_name_to_analyze)
+        st.pyplot(fig, clear_figure=False)
+
+        # Set Up filters
+        st.write("High Pass Configuration")
+        left, right = st.columns(2)
+        freq_input = left.text_input(label="freq", value="2")
+        corners_input = right.text_input(label="corners", value="4")
+        st.write("Band Pass Configuration")
+        left, right = st.columns(2)
+        min_freq = left.text_input(label="min_freq", value="0.4")
+        max_freq = right.text_input(label="max_freq", value="1.0")
+        last_data_st, cft = None, None
+
+        left, middle, right = st.columns(3)
+
+        if left.button("apply highpass", use_container_width=True):
+            hp_st_data = apply_highpass_filter(selected_sample_st, float(freq_input), int(corners_input))
+            fig, _ = plot_values_from_special_content(hp_st_data, file_name_to_analyze, title="High Pass Filter", plot_spectogram=True)
+            st.session_state["fig_test"] = fig
+            st.session_state["last_data_st_test"] = hp_st_data.copy()
+
+        if middle.button("apply bandpass", use_container_width=True):
+            bp_st_data = apply_bandpass_filter(selected_sample_st, float(min_freq), float(max_freq))
+            fig, _ = plot_values_from_special_content(bp_st_data, file_name_to_analyze, title="Band Pass Filter", plot_spectogram=True)
+            st.session_state["fig_test"] = fig
+            st.session_state["last_data_st_test"] = bp_st_data.copy()
+
+        if right.button("hp + bp", use_container_width=True):
+            hp_st_data = apply_highpass_filter(selected_sample_st, float(freq_input), int(corners_input))
+            bp_st_data = apply_bandpass_filter(hp_st_data, float(min_freq), float(max_freq))
+            fig, _ = plot_values_from_special_content(bp_st_data, file_name_to_analyze, title="HP + BP Filter", plot_spectogram=True)
+            st.session_state["fig_test"] = fig
+            st.session_state["last_data_st_test"] = bp_st_data.copy()
+
+        if st.session_state.get("fig_test"):
+            st.pyplot(st.session_state.get("fig_test"), clear_figure=False)
+
+            # Apply algorithm STA/LTA
+            st.write("STA/LTA Configuration")
+            left, right = st.columns(2)
+            sta_len = left.text_input(label="sta len", value="120")
+            lta_len = right.text_input(label="lta len", value="600")
+            if st.button("Apply STA/LTA", use_container_width=True):
+                if st.session_state.get("last_data_st_test") is not None:
+                    cft, fig, ax = apply_lst_sta_algo_and_plot_char_func(st.session_state.get("last_data_st_test"),
+                                                                         float(sta_len), float(lta_len))
+                    st.session_state["cft_test"] = cft
+                    st.session_state["fig_cft_test"] = fig
+
+            if st.session_state.get("fig_cft_test"):
+                st.pyplot(st.session_state.get("fig_cft_test"), clear_figure=False)
+
+                st.write("On/Off points configuration")
+                left, right = st.columns(2)
+                trh_on = left.text_input(label="thr on", value="4")
+                trh_off = right.text_input(label="thr off", value="1.5")
+                if st.button("Calculate On/Off points", use_container_width=True):
+                    if st.session_state.get("cft_test") is not None:
+                        triggers, fig, ax = calculate_on_off_cft_and_plot(st.session_state.get("last_data_st_test"),
+                                                                          st.session_state.get("cft_test"),
+                                                                          float(trh_on), float(trh_off))
+
+                        if not triggers:
+                            st.warning("Method could not detect the triggers change parameters")
+
+                        st.session_state["triggers_test"] = triggers
+                        st.pyplot(fig, clear_figure=False)
+                        if triggers:
+                            st.write("Detection")
+                            predicted_arrival_time = calculate_predicted_arrival_time(st.session_state.get('last_data_st_test'), triggers)
+                            st.write(f"Detected: {predicted_arrival_time}")
+
+def training_data_visualization_energy_peak_review():
+    import streamlit as st
+
+    folder_selection = st.selectbox(label="Select what data you want to see", options=["mars", "lunar"])
+
+    list_data_df = get_list_of_data_training(folder_selection, "csv")
+
+    # Configure grid options using GridOptionsBuilder
+    list_data_df_builder = GridOptionsBuilder.from_dataframe(list_data_df)
+    list_data_df_builder.configure_pagination(enabled=True)
+    list_data_df_builder.configure_selection(selection_mode='single', use_checkbox=False)
+    grid_options = list_data_df_builder.build()
+
+    # Display AgGrid
+    st.write("List of Data for Mars/Lunar")
+    list_data_df_return_value = AgGrid(list_data_df, gridOptions=grid_options)
+
+    if list_data_df_return_value["selected_data"] is not None:
+        selected_value = list_data_df_return_value["selected_data"]["path"].values[0]
+        selected_df = load_data(selected_value)
+        extension = "mseed"
+        match_filename = get_match_filename_training(selected_df["filename"].to_list(), folder_selection, extension)
+
+        st.write(f"List of Data for selected value: {selected_value}")
+
+        # Configure grid options using GridOptionsBuilder
+        selected_df_builder = GridOptionsBuilder.from_dataframe(selected_df)
+        selected_df_builder.configure_pagination(enabled=True)
+        selected_df_builder.configure_selection(selection_mode='single', use_checkbox=False)
+        grid_options = selected_df_builder.build()
+        selected_df_return_value = AgGrid(selected_df, gridOptions=grid_options)
+
+        # Plot current data
+        if selected_df_return_value["selected_data"] is not None:
+            selected_value = selected_df_return_value["selected_data"]["filename"].values[0]
+            selected_arrival_time = selected_df_return_value["selected_data"]["time_abs(%Y-%m-%dT%H:%M:%S.%f)"].values[0]
+            selected_arrival_time = datetime.strptime(selected_arrival_time, '%Y-%m-%dT%H:%M:%S.%f')
+            st.write(f"Plot of the time series for file {selected_value}")
+            file_name_to_analyze = match_filename[selected_value]
+            selected_sample_st = load_special_file(file_name_to_analyze)
+            #fig, ax = plot_values_from_special_content(selected_sample_st, file_name_to_analyze, selected_arrival_time, plot_real=True)
+            #st.pyplot(fig, clear_figure=False)
+
+page_names_to_funcs = {
+    "trainig sta/dta interactive visualization": training_data_visualization_review,
+    "test sta/dta interactive visualization": testing_data_visualization_review,
+    "training energy peak interactive visualization": training_data_visualization_energy_peak_review,
+}
+
+demo_name = st.sidebar.selectbox("Choose what you want to see", page_names_to_funcs.keys())
+page_names_to_funcs[demo_name]()
---
+++ src/data_utils.py
@@ -0,0 +1,216 @@
+import pandas as pd
+import numpy as np
+import os
+import glob
+import streamlit as st
+import obspy as obspy
+import matplotlib.pyplot as plt
+from obspy.signal.invsim import cosine_taper
+from obspy.signal.filter import highpass
+from obspy.signal.trigger import classic_sta_lta, plot_trigger, trigger_onset
+from typing import List
+from scipy import signal
+from matplotlib import cm
+
+BASE_PATH: str = "data"
+
+@st.cache_data
+def load_data(file_path: str):
+    try:
+        df = pd.read_csv(file_path)
+        return df
+    except:
+        st.error("data does not exists")
+
+def load_special_file(file_path: str):
+    try:
+        st_values = obspy.read(file_path)
+        return st_values
+    except:
+        st.error(f"data does not exists: {file_path}")
+
+def plot_values_from_special_content(st_values, test_filename, arrival_time=None, title="",  plot_real=False, plot_spectogram=False):
+    # This is how you get the data and the time, which is in seconds
+    tr = st_values.traces[0].copy()
+    tr_times = tr.times()
+    tr_data = tr.data
+
+
+    # Initialize figure
+    if plot_spectogram:
+        # To better see the patterns, we will create a spectrogram using the scipy function
+        # It requires the sampling rate, which we can get from the miniseed header as shown a few cells above
+        f, t, sxx = signal.spectrogram(tr_data, tr.stats.sampling_rate)
+        fig = plt.figure(figsize=(10, 10))
+        ax = plt.subplot(2, 1, 1)
+        ax2 = plt.subplot(2, 1, 2)
+        vals = ax2.pcolormesh(t, f, sxx, cmap=cm.jet, vmax=5e-17)
+        ax2.set_xlim([min(tr_times),max(tr_times)])
+        ax2.set_xlabel(f'Time (Day Hour:Minute)', fontweight='bold')
+        ax2.set_ylabel('Frequency (Hz)', fontweight='bold')
+        if plot_real and arrival_time is not None:
+            starttime = tr.stats.starttime.datetime
+            arrival = (arrival_time - starttime).total_seconds()
+            ax2.axvline(x=arrival, c='red')
+        cbar = plt.colorbar(vals, orientation='horizontal')
+        cbar.set_label('Power ((m/s)^2/sqrt(Hz))', fontweight='bold')
+    else:
+        fig,ax = plt.subplots(1,1,figsize=(10,3))
+
+    # Plot trace
+    ax.plot(tr_times,tr_data)
+
+    if plot_real and arrival_time is not None:
+        starttime = tr.stats.starttime.datetime
+        arrival = (arrival_time - starttime).total_seconds()
+        # Plot where the arrival time is
+        # Mark detection
+        ax.axvline(x = arrival, color='red',label='Rel. Arrival')
+        ax.legend(loc='upper left')
+
+    # Make the plot pretty
+    ax.set_xlim([min(tr_times),max(tr_times)])
+    ax.set_ylabel('Velocity (m/s)')
+    ax.set_xlabel('Time (s)')
+    ax.set_title(f'{title} {test_filename}', fontweight='bold') 
+    return fig, ax
+
+
+@st.cache_data
+def search_in_list(data: str, list_: List[str]):
+    for i, value in enumerate(list_):
+        if data in value:
+            return i
+
+@st.cache_data
+def get_match_filename_training(filenames: List[str], asteroid_path: str, extension: str | None = None):
+    path = os.path.join(BASE_PATH, asteroid_path)
+    if extension is None:
+        extension = "csv"
+    result = [y for x in os.walk(path) for y in glob.glob(os.path.join(x[0], f'*.{extension}'))]
+    result_filter = [r for r in result if "training" in r  and "data" in r]
+    results_mapping = {}
+    for filename in filenames:
+        index = search_in_list(filename, result_filter)
+        if index is not None:
+            results_mapping[filename] = result_filter[index]
+    return results_mapping
+
+@st.cache_data
+def get_list_of_data_training(asteroid_path: str, extension: str | None = None):
+    path = os.path.join(BASE_PATH, asteroid_path)
+    if extension is None:
+        extension = "csv"
+    result = [y for x in os.walk(path) for y in glob.glob(os.path.join(x[0], f'*.{extension}'))]
+    result_filter = [r for r in result if "training" in r  and "catalogs" in r]
+    data_df = pd.DataFrame(result_filter, columns=["path"])
+    return data_df
+
+@st.cache_data
+def get_list_of_data_testing(asteroid_path: str, extension: str | None = None):
+    path = os.path.join(BASE_PATH, asteroid_path)
+    if extension is None:
+        extension = "csv"
+    result = [y for x in os.walk(path) for y in glob.glob(os.path.join(x[0], f'*.{extension}'))]
+    result_filter = [r for r in result if "test" in r  and "data" in r]
+    data_df = pd.DataFrame(result_filter, columns=["path"])
+    return data_df
+
+def apply_bandpass_filter(st, minfreq=0.5, maxfreq=1.0):
+    # Going to create a separate trace for the filter data
+    st_filt = st.copy()
+    st_filt.filter('bandpass',freqmin=minfreq,freqmax=maxfreq)
+    return st_filt
+
+
+def apply_highpass_filter(st, freq=3, corners=4):
+    # Going to create a separate trace for the filter data
+    st_filt = st.copy()
+    st_filt.filter('highpass',freq=freq,corners=corners)
+    return st_filt
+
+def apply_lst_sta_algo_and_plot_char_func(st_values, sta_len=120, lta_len=600):
+    # Sampling frequency of our trace
+    tr = st_values.traces[0].copy()
+    df = tr.stats.sampling_rate
+    tr_times = tr.times()
+    tr_data = tr.data
+
+    # Run Obspy's STA/LTA to obtain a characteristic function
+    # This function basically calculates the ratio of amplitude between the short-term 
+    # and long-term windows, moving consecutively in time across the data
+    cft = classic_sta_lta(tr_data, int(sta_len * df), int(lta_len * df))
+
+    # Plot characteristic function
+    fig, ax = plt.subplots(1,1,figsize=(12,3))
+    ax.plot(tr_times,cft)
+    ax.set_xlim([min(tr_times),max(tr_times)])
+    ax.set_xlabel('Time (s)')
+    ax.set_ylabel('Characteristic function')
+
+    return cft, fig, ax
+
+
+def calculate_on_off_cft_and_plot(st_values, cft, thr_on=4, thr_off=1.5):
+    # Play around with the on and off triggers, based on values in the characteristic function
+    tr = st_values.traces[0].copy()
+    df = tr.stats.sampling_rate
+    tr_times = tr.times()
+    tr_data = tr.data
+
+    on_off = np.array(trigger_onset(cft, thr_on, thr_off))
+    triggers_list = []
+    # The first column contains the indices where the trigger is turned "on". 
+    # The second column contains the indices where the trigger is turned "off".
+
+    # Plot on and off triggers
+    fig, ax = plt.subplots(1,1,figsize=(12,3))
+    for i in np.arange(0,len(on_off)):
+        triggers = on_off[i]
+        ax.axvline(x = tr_times[triggers[0]], color='red', label='Trig. On')
+        ax.axvline(x = tr_times[triggers[1]], color='purple', label='Trig. Off')
+        triggers_list.append(triggers)
+
+    # Plot seismogram
+    ax.plot(tr_times,tr_data)
+    ax.set_xlim([min(tr_times),max(tr_times)])
+    ax.legend()
+    return triggers_list, fig, ax
+
+def plot_real_trigger_axvline(st_values, arrival_time, ax, fig):
+    # This is how you get the data and the time, which is in seconds
+    tr = st_values.traces[0].copy()
+    tr_times = tr.times()
+    tr_data = tr.data
+    starttime = tr.stats.starttime.datetime
+    arrival = (arrival_time - starttime).total_seconds()
+    # Plot where the arrival time is
+    # Mark detection
+    ax.axvline(x = arrival, color='green',label='Real Arrival')
+    ax.legend()
+    return fig, ax
+
+def calculate_arrival_time(st_values, arrival_time):
+    # This is how you get the data and the time, which is in seconds
+    tr = st_values.traces[0].copy()
+    tr_times = tr.times()
+    tr_data = tr.data
+    starttime = tr.stats.starttime.datetime
+    arrival = (arrival_time - starttime).total_seconds()
+    return arrival
+
+def calculate_predicted_arrival_time(st_values, triggers):
+    # This is how you get the data and the time, which is in seconds
+    tr = st_values.traces[0].copy()
+    df = tr.stats.sampling_rate
+    tr_times = tr.times()
+    tr_data = tr.data
+
+    triggers_ = triggers[0]
+    return tr_times[triggers_[0]]
+
+def calculate_root_mean_square_error(expected, actual):
+    if (isinstance(expected, float) and isinstance(actual, float)):
+        return np.sqrt((expected - actual)**2)
+    else:
+        return np.sqrt((expected - actual) ** 2/len(expected))
